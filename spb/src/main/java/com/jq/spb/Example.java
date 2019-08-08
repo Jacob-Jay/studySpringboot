64448b589fca77180d291a3acc857f78282e9698
NoWaterFish <819657451@qq.com>
2019-08-09 00:03:40 +0800

    springboot 笔记

---
+++ spb/src/main/java/com/jq/spb/Example.java
@@ -0,0 +1,24 @@
+package com.jq.spb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jiangqing
+ * @version 1.0
+ * @since 2019/8/8 23:37
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+}
