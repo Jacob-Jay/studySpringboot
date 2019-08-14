7641a377dfeab691f3c33671cb40e414aab89fc2
蒋晴 <819657451@qq.com>
2019-08-14 17:02:11 +0800

    数据库相关版本

---
+++ spb4/src/main/java/com/jq/spb4/Spb4Application.java
@@ -0,0 +1,13 @@
+package com.jq.spb4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spb4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spb4Application.class, args);
+	}
+
+}
---
+++ spb4/src/main/java/com/jq/spb4/controller/MybatisController.java
@@ -0,0 +1,9 @@
+package com.jq.spb4.controller;
+
+/**
+ * @author Jiangqing
+ * @version 1.0
+ * @since 2019-08-14 17:00
+ */
+public class MybatisController {
+}
